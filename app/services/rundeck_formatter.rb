193a56181316b33b0e1f5384b114d79db351526d
Nathaniel Cook <nathanielc@qualtrics.com>
2015-02-18 13:44:47 -0700

    make quey params override instead of replace

--- app/services/rundeck_formatter.rb
+++ app/services/rundeck_formatter.rb
@@ -1,26 +1,27 @@
 class RundeckFormatter
   attr_reader :host
-  attr_reader :params
+  attr_reader :query_params
 
-  delegate :comment, :name, :arch, :environment, :os, :facts_hash, :puppetclasses_names, :to => :params, :to => :host
+  delegate :comment, :name, :arch, :environment, :os, :facts_hash, :puppetclasses_names, :params, :to => :host
   delegate :logger, :to => :Rails
 
-  def initialize(host, params)
+  def initialize(host, query_params)
     @host = host
-    @params = params
+    @query_params = query_params
   end
 
     def output
     rdecktags = puppetclasses_names.map { |k| "class=#{k}" }
-    unless params['rundeckfacts'].empty?
-      rdecktags += params['rundeckfacts'].gsub(/\s+/, '').split(',').map { |rdf| "#{rdf}=" + (facts_hash[rdf] || 'undefined') }
+    merged_params = params.merge(query_params)
+    unless merged_params['rundeckfacts'].empty?
+      rdecktags += merged_params['rundeckfacts'].gsub(/\s+/, '').split(',').map { |rdf| "#{rdf}=" + (facts_hash[rdf] || 'undefined') }
     end
 
     {name => {'description' => comment, 'hostname' => name, 'nodename' => name,
               'Environment' => environment.name,
               'osArch' => arch.name, 'osFamily' => os.family, 'osName' => os.name,
               'osVersion' => os.release, 'tags' => rdecktags,
-              'username' => params['rundeckuser'] || 'root'}
+              'username' => merged_params['rundeckuser'] || 'root'}
     }
   rescue => e
     logger.warn "Failed to fetch rundeck info for #{to_s}: #{e}"
